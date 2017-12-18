23678a6fbb540ca2a8e063ea92f3b43613c9ea7a
Atif Mahmud <atifmahmud95@gmail.com>
2017-12-18 20:30:21 +0600

    runs, doesn't retrieve

--- gradeCheck.js
+++ gradeCheck.js
@@ -4,21 +4,43 @@
  * Date: 17th December, 2017
  */
 
-var url = "https://ssc.adm.ubc.ca/sscportal/servlets/SRVSSCFramework?function=SessGradeRpt";
+
+console.log("start");
+
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app = express();
+var grades = [];
+
+app.get('/scrape', function(res, req){
+
+    console.log("in app.get");
+    var url = 'https://ssc.adm.ubc.ca/sscportal/servlets/SRVSSCFramework?function=SessGradeRpt';
+    request(url, function(error, response, html){
+
+        console.log("in request");
+
+        if(!error){
+            
+            console.log("in !error");
+
+            var $ = cheerio.load(html);
+
+            $('#allSessionsGrades td').each(function(){
+                var grade = $(this).find('.sortarrow').html();
+                grades.push(grade);
+                console.log(grade);
+            });
+        }
 
 
-/* Logs in to the Grades portal */
-window.open(url, "_self");
 
-setTimeout(callback, 2000);
+    });
 
-function callback(){
-var loginNameBox = document.getElementById("username");
-var passwordBox = document.getElementById("password");
-var loginName = prompt("Please enter login name");
-var password = prompt("Please enter password");
-loginNameBox.value = loginName;
-passwordBox.value = password;
-var continueButton = document.getElementsByClassName("ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only btn")[0]; 
-continueButton.click();
-}
+
+});
+
+app.listen('8081');
+exports = module.exports = app;
