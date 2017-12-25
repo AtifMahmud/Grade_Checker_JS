bf25216a2957d681fb484490c058d374baff6f24
Atif Mahmud <atifmahmud95@gmail.com>
2017-12-25 10:19:16 +0600

    Stores file

--- gradeCheck.js
+++ gradeCheck.js
@@ -5,11 +5,12 @@
  */
 
 var url = "https://ssc.adm.ubc.ca/sscportal/servlets/SRVAcademicRecord?context=html";
-var username = 'johnDoe';
-var password = 'I<3UBC';
+var username = 'johnDoes';
+var password = 'ihunbau';
 
 var casper = require('casper').create();
 var x = require('casper').selectXPath;
+var fs = require('fs');
 
 casper.userAgent('Chrome/37.0.2062.120');
 casper.start(url);
@@ -17,14 +18,25 @@ casper.start(url);
 casper.then(function(){
     this.sendKeys('#username', username);
     this.sendKeys('#password', password);
-    casper.capture('screen2.png');
     console.log('Username and password inserted');
 });
 
 casper.thenClick(x('//*[@id="login"]/input[6]'), function (){
-    casper.capture('screen2.png');
     console.log('Clicked Button');
+    casper.capture('After_Login.png');
 });
 
+casper.then(function(){
+
+    var gradeTable = this.getHTML('table#allSessionsGrades', true);
+    
+    if(!fs.exists('gradeTable.html')){
+        fs.write('gradeTable.html', gradeTable, 'w');
+    }
+
+});
+
+/* Note take screenshot when new scores available, therefore no need to JSON */
 casper.run();
+
 
